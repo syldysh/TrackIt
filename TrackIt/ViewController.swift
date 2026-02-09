583537e810ef6a435d48e3c2b95e00b999e7cae3
Syldys <smshogzhal@edu.hse.ru>
2026-02-09 21:32:44 +0300

    Initial commit

---
+++ TrackIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackIt
+//
+//  Created by Сылдыс Шогжал on 09.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
